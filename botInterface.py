77a30d933f28c1ce56cf17699453f0287950931d
Ricardo Perez <rperezalbores@hotmail.com>
2017-09-10 08:28:25 +0400

    added read input parameters from file

--- botInterface.py
+++ botInterface.py
@@ -3,26 +3,42 @@ Created on Sep 4, 2017
 
 @author: ricardo
 '''
+import time
+from dateutil import parser  # covenient lib to manipulate dates
 
-filename = 'botConfig.cfg'
-fileObj = open(filename)
-var = {}
-for line in fileObj:
-    line = line.strip()
-    if not line.startswith('#'):
-        key_value = line.split('=')
-        if len(key_value) == 2:
-            var[key_value[0].strip()] = key_value[1].strip()
+try:
+    filename = 'botConfig.cfg'
+    fileObj = open(filename)
+    var = {}
+    for line in fileObj:
+        line = line.strip()
+        if not line.startswith('#'):
+            key_value = line.split('=')
+            if len(key_value) == 2:
+                var[key_value[0].strip()] = key_value[1].strip()
+    
+    var['pair'] = str(var['pair'])
+    var['exchange'] = str(var['exchange'])
+    start = parser.parse(var['start'])              # start is now DateTime format
+    var['start'] = time.mktime(start.timetuple())   # now converted into unix time
+    end = parser.parse(var['end'])                  # end is now DateTime format
+    var['end'] = time.mktime(start.timetuple())     # now converted into unix time
+    
+    if  var['end'] <= var['start']:
+        print var['end'], ' needs to be later than ', var['start']
+        exit(0)
+    
+    var['period'] = int(var['period'])
+    if var['period'] not in [300, 900, 1800, 7200, 1440, 86400]:  
+        print var['period'], ' must be one of 300, 900, 1800, 7200, 14400, and 86400' 
+        exit(0)
+    
+    var['kissLow'] = int(var['kissLow'])
+    var['kissMA'] = int(var['kissMA'])
+    var['kissHigh'] = int(var['kissHigh'])
+    var['kissStoploss'] = int(var['kissHigh'])
+                 
+except Exception, e:
+    print 'something wrong on the configuration file: wrong variables ', str(e)
 
-var['pair'] = str(var['pair'])
-var['exchange'] = str(['exchange'])
-start = '01-08-2017 22:01'
-end = '01-08-2017 23:01'
-var['period'] = int(var['period'])                      
-
-
-kissLow = 2
-kissMA = 3
-kissHigh = 3
-kissStoploss = 95            
-
+print var
--- bot_history.py
+++ bot_history.py
@@ -5,51 +5,82 @@ Created on Sep 2, 2017
 '''
 import time
 import datetime
+from dateutil import parser  # covenient lib to manipulate dates
 import logging
-import numpy as np
-from poloniex import Poloniex, PoloniexPublic
-
+from poloniex import PoloniexPublic
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.ticker as mticker
+# import matplotlib.dates as mdates
+# import matplotlib.ticker as mticker
 from matplotlib.finance import candlestick2_ohlc
-from matplotlib.dates import DateFormatter
+# from matplotlib.dates import DateFormatter
 from matplotlib import style
-import plotly.plotly as py
-import plotly.graph_objs as go
-from trade_history import trade_history, kissBB
+# import plotly.plotly as py
+# import plotly.graph_objs as go
+from strategy import kissBB
 
-        
-class ledger:
+style.use('ggplot')
+logging.basicConfig(filename='strategy.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
+   
+class ledger:     # #this class holds the bot transactions. It also keeps track of open position (ongoingOrder = True) and its price (purchasePrice)
     def __init__(self):
-        self.ledger = []    # [{'date': date, 'type': 'buy/sell', 'price' : price}]
+        self.ledger = []    # list of dictionaries  [{'date': date, 'type': 'buy/sell', 'price' : price}]
         self.ongoingOrder = False
         self.purchasePrice = 0
  
-    def write (self, newEntry):
+    def write (self, newEntry):     # this function adds an entry to the transaction ledger
         self.ledger = self.ledger + newEntry
         
-    def setPurchaseprice (self, purchasePrice):
+    def setPurchaseprice (self, purchasePrice): # this function sets the purchasePrice of our last open position
         self.purchasePrice = purchasePrice
         
-    def setOngoingorder (self, ongoingOrder):
+    def setOngoingorder (self, ongoingOrder):   # ongoingOrder=True means we have an open position, which we are looking to close. If False, then no open posotions
         self.ongoingOrder = ongoingOrder
 
         
-        
-    
-    
-style.use('ggplot')
-logging.basicConfig(filename='trade_history.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
-
-
-def pullData(pair, period, uStart, uEnd):
+def readInputvariables():   # this function reads the input variables from file botConfig.cfg.
     try:
+        filename = './botConfig.cfg'
+        fileObj = open(filename)
+        var = {}
+        for line in fileObj:
+            line = line.strip()
+            if not line.startswith('#'):
+                key_value = line.split('=')
+                if len(key_value) == 2:
+                    var[key_value[0].strip()] = key_value[1].strip()
+        
+        var['pair'] = str(var['pair'])
+        var['exchange'] = str(var['exchange'])
+        start = parser.parse(var['start'])              # start is now DateTime format
+        var['start'] = time.mktime(start.timetuple())   # now converted into unix time
+        end = parser.parse(var['end'])                  # end is now DateTime format
+        var['end'] = time.mktime(end.timetuple())       # now converted into unix time       
+        if  var['end'] <= var['start']:
+            print var['end'], ' needs to be later than ', var['start']
+            exit(0)   
+        var['period'] = int(var['period'])
+        if var['period'] not in [300, 900, 1800, 7200, 1440, 86400]:  
+            print var['period'], ' must be one of 300, 900, 1800, 7200, 14400, and 86400' 
+            exit(0)
+        var['kissLow'] = int(var['kissLow'])
+        var['kissMA'] = int(var['kissMA'])
+        var['kissHigh'] = int(var['kissHigh'])
+        var['kissStoploss'] = int(var['kissHigh'])        
+        return var
+                 
+    except Exception, e:
+        print 'something wrong on the configuration file: wrong variables ', str(e)
+   
+
+def pullData(pair, period, uStart, uEnd):   # this function retrieves data from the exchange
+    try:
+        print uStart, uEnd
         pol = PoloniexPublic()
 
-        # dataAPI = pol.returnTradeHistory(pair)  -> returns history
-        dataAPI = pol.returnChartData(pair, period, uStart, uEnd)
+        # dataAPI = pol.returnTradeHistory(pair)  -> returns last 200 history entries 
+        dataAPI = pol.returnChartData(pair, period, uStart, uEnd)  # returns entries between two dates, at the given period:300, 900, 1800 (sec)
         print dataAPI
         df = pd.DataFrame(dataAPI)
         df['date'] = pd.to_datetime(df['date'], unit='s')  # converts UNIX dates returned by Poloniex into human readable dates
@@ -106,33 +137,32 @@ def graphStock(df, pair):
         print 'graphStock ', str(e)
 
 
+# ## start the program
 
-pair = 'USDT_BTC'
-start = datetime.date(2017, 9, 1)
-end = datetime.date(2017, 9, 3)
-uStart = time.mktime(start.timetuple())
-uEnd = time.mktime(end.timetuple())
+if __name__ == '__main__':
 
-data = pullData(pair, 300, uStart, uEnd)
-dataF = analysis(data)
-ledger = ledger()
-
-i = 0
-while i < len(dataF):
-    if i >= 20:
-        trade = kissBB(dataF.ix[i - 20:i], ledger, i)
-        if trade:
-            print i
- 
-    logging.info('i={0} \n{1}'.format(i, dataF.loc[i]))
-    i = i + 1
-
-df = pd.DataFrame(ledger.ledger)        
-print df
-print 'profit', df['profit'].sum()
-
-graphStock(dataF, pair)
-
-# trade(dataF.ix[20])
-# dataF.plot(x='date', y='weightedAverage')
-# plt.show()
+    var = readInputvariables()              # reads input variables from file
+                                            # fetches data from the exchange
+    data = pullData(var['pair'], var['period'], var['start'], var['end'])
+    dataF = analysis(data)                  # calculates technical analysis on the fetched data
+    ledger = ledger()                       # instantiates the ledger
+        
+    i = 0
+    while i < len(dataF):
+        if i >= 20:
+            trade = kissBB(dataF.ix[i - 20:i], ledger, i)
+            if trade:
+                print i
+     
+        logging.info('i={0} \n{1}'.format(i, dataF.loc[i]))
+        i = i + 1
+    
+    df = pd.DataFrame(ledger.ledger)               
+    print df
+    print 'profit', df['profit'].sum()
+    
+    graphStock(dataF, var['pair'])
+    
+    # trade(dataF.ix[20])
+    # dataF.plot(x='date', y='weightedAverage')
+    # plt.show()
